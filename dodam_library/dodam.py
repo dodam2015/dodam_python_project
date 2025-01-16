18685acdc32be4955290a08e842894334d5f21b2
dodam2015 <145336038+dodam2015@users.noreply.github.com>
2025-01-16 12:01:46 +0900

    Add files via upload

---
+++ dodam_library/dodam.py
@@ -0,0 +1,28 @@
+# dodam.py
+import time
+import datetime
+
+
+def dprint(a):
+    print(a)
+def dodamisgod():
+    print("yes I'm god dodam2015no1@naver.com")
+def wait(b):
+    b=int(b)
+    time.sleep(b)
+def strtoasciicode(a):
+    dprint(ord(str(a)))
+def asciicodetostr(a):
+    dprint(chr(int(a)))
+def replace(a,b,c):
+    d=a.replace(b,c)
+    print(d)
+def stop():
+    quit()
+def nowtime():
+    print(datetime.datetime.now())
+def dreturn(a):
+    return a
+def dodam_version():
+    print('dodam_version 1.0.0')
+
---
+++ dodam_library/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dodam',
+    version='1.0',
+    packages=find_packages(),
+    description='dodamdodam',
+    author='Doyun',
+    author_email='dodam2015no1@naver.com',
+    url='https://github.com/dodam2015/dodam_python_project',  # 프로젝트 URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
